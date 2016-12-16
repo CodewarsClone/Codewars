412a8916d2b5c4edbd3dfa8f35097629ad265e25
Joshua Baert <jbghoring@gmail.com>
2016-12-15 20:37:40 -0700

    Commit & Push to server for test

--- controllers/testCtrl.js
+++ controllers/testCtrl.js
@@ -6,6 +6,7 @@ const exec = require('child_process').exec;
 
 exec('docker run --rm codewars/node-runner run -l javascript -c "var a = 1;" -t cw -f "Test.assertEquals(a, 1)"', (err, stdout, stderr)=> {
 	if (err) console.log('err', err);
-	if (stdout) console.log('stdout', stdout);
-	if (stderr) console.log('stderr', stderr);
+	console.log(typeof stdout);
+	console.log('stdout', stdout);
+	console.log('stderr', stderr);
 });
