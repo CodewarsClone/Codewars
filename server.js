72d490861b6abec8c61f8af01e392f8745e141b4
alexisgraff5 <lex.graff@gmail.com>
2016-12-16 14:18:43 -0700

    npm installed passport and passport-github2. each person will need to do another npm install. created two controllers

--- server.js
+++ server.js
@@ -4,19 +4,36 @@ var session = require('express-session');
 var cors = require('cors');
 var config = require('./config.js');
 var massive = require('massive');
+var passport = require('passport');
+var GithubStrategy = require('passport-github2').Strategy;
 var connectionString = config.connectionString;
+
+passport.use(new GithubStrategy({
+  clientID: config.githubId,
+  clientSecret: config.githubSecret,
+  callbackURL: '/auth/github/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  // code goes here
+    // go to database and look for profile.id
+    // create user using profile.id
+    return done(null/*error*/, profile/*info that goes on session*/);
+}));
+
+passport.serializeUse((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done)  => {
+  done(null, obj);
+});
 
 var app = module.exports = express();
 
 var massiveInstance = massive.connectSync({connectionString : connectionString});
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 app.use(bodyParser.json());
 app.use(cors());
-
-app.set('db', massiveInstance);
-
-var db = app.get('db');
 
 app.use(session({
   secret: config.sessionSecret,
@@ -24,6 +41,24 @@ app.use(session({
   resave: false
 }));
 
-app.listen(config.port, function() {
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('db', massiveInstance);
+var db = app.get('db');
+var userCtrl = require('./controllers/userCtrl');
+
+app.get('/auth/github', passport.authenticate('github'));
+app.get('/auth/github/callback', passport.authenticate('github', {
+	successRedirect: '/me',
+	failureRedirect: '/login'
+}), (req, res) => {
+	console.log(req.session);
+});
+
+
+
+
+app.listen(config.port, () => {
   console.log('listening to port', config.port);
 });
