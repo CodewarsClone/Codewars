f70f1f125d668141da0316309424c0e1087ed45b
alexisgraff5 <lex.graff@gmail.com>
2016-12-19 12:34:22 -0700

    added endpoints to server.js

---
+++ controllers/kataCtrl.js
@@ -0,0 +1,27 @@
+const app = require('../server');
+const db = app.get('db');
+
+module.exports = {
+    getKata: (res, req, next) => {
+
+    },
+
+	postSolution: (res, req, next) => {
+
+    },
+
+    getRandomKata: (res, req, next) => {
+
+    },
+
+    getCompletedKatas: (res, req, next) => {
+
+    },
+
+    getKataSolutions: (res, req, next) => {
+
+    },
+
+    
+
+}
