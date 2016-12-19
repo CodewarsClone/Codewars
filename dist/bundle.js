2fc41da9c05de4bb3b7b9df849ce0d24dbf0e196
Steven Nagie <sanagie@yahoo.com>
2016-12-19 14:22:48 -0700

    Begin working on training page styling.

--- dist/bundle.js
+++ dist/bundle.js
@@ -61,9 +61,9 @@ angular.module('app').controller('homeCtrl', function ($scope, $state, mainServi
 angular.module('app').controller('trainingCtrl', function ($scope, $state, mainService) {
 
   $scope.sendSolution = function (input) {
-    // mainService.getTest(input).then((response) => console.log(response));
     input = input.replace(/\n/g, " ");
     console.log(input);
+    // mainService.getTest(input).then((response) => console.log(response));
   };
 });
 //# sourceMappingURL=bundle.js.map
