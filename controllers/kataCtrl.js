f46715b7637b5ba8767a0048bdb6b2a79c139881
alexisgraff5 <lex.graff@gmail.com>
2016-12-19 14:23:02 -0700

    created sql files without code in them

--- controllers/kataCtrl.js
+++ controllers/kataCtrl.js
@@ -3,23 +3,39 @@ const db = app.get('db');
 
 module.exports = {
     getKata: (req, res, next) => {
+        db.get_kata([], (err, user) => {
 
+        })
+        db.read.kata_by_id([], (err, user) => {
+
+        })
     },
 
 	postSolution: (req, res, next) => {
-
+        db.post_solution([], (err, user) => {
+            
+        })
     },
 
     getRandomKata: (req, res, next) => {
-
+        db.get_random_kata([], (err, user) => {
+            
+        })
+        db.get_random_by_kyu([], (err, user) => {
+            
+        })
     },
 
     getCompletedKatas: (req, res, next) => {
-
+        db.get_completed_katas([], (err, user) => {
+            
+        })
     },
 
     getKataSolutions: (req, res, next) => {
-
+        db.get_kata_solutions([], (err, user) => {
+            
+        })
     },
 
     
