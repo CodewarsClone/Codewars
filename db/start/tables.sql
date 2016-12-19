3f0af57ce6ecf63c3a337d01d17c30a2f23d6c0f
Joshua Baert <jbghoring@gmail.com>
2016-12-19 14:37:59 -0700

    Testing from database.

--- db/start/tables.sql
+++ db/start/tables.sql
@@ -6,17 +6,19 @@ CREATE TABLE users (
   first_name varchar(40),
   last_name varchar(40),
   email varchar(255),
-  username varchar (40)
+  username varchar (40),
+  picutre_url text
 
 );
 
-CREATE TABLE kata (
+CREATE TABLE katas (
   id serial primary key,
   kyu integer not null,
-  test text not null,
+  test_script text not null,
   description text,
   starter_code text not null,
-  name varchar(255)
+  name varchar(255),
+  examples text
 );
 
 CREATE TABLE solutions (
@@ -26,7 +28,7 @@ CREATE TABLE solutions (
   script text not null
 );
 
-CREATE TABLE rating (
+CREATE TABLE ratings (
   id serial primary key,
   user_id integer references users(id),
   kata_id integer references kata(id),
@@ -34,15 +36,15 @@ CREATE TABLE rating (
   liked boolean not null
 );
 
-INSERT INTO users (github_id, first_name, last_name, email, username)
-VALUES (null, 'bob', 'smith', 'bob@smith.com', 'bobIScool');
+INSERT INTO users (github_id, first_name, last_name, email, username, picture_url)
+VALUES (null, 'bob', 'smith', 'bob@smith.com', 'bobIScool', null);
 
-INSERT INTO kata (kyu, test, description, starter_code, name)
-VALUES (8, 'Test.assertEquals(a, 1)', 'a should equal 1', 'var a = 1', 'Sumbit This');
+INSERT INTO katas (kyu, test_script, description, starter_code, name, examples)
+VALUES (8, 'Test.assertEquals(a, 1)', 'a should equal 1', 'var a = 1', 'Sumbit This', null);
 
 INSERT INTO solutions (user_id, kata_id, script)
 VALUES (1, 1, 'var a = 1'),
 (1,1, 'let a = 1');
 
-INSERT INTO rating (user_id, solution_id, liked)
+INSERT INTO ratings (user_id, solution_id, liked)
 VALUES (1, 1, true);
