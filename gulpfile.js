71d3f77787c30b2409909b6e3a9b005d5dad743c
Sterling Chin <sterling.chin@gmail.com>
2016-12-19 11:30:07 -0700

    reset.css file added, Started Directives

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,18 @@
-
-  //1. Make gulpfile
+//1. Make gulpfile
 //2. npm i all dependencies
 //3. check all folder paths used in gulpfile
 //4. Update index.html
 //5. Run gulp watch
 
 var gulp = require('gulp'),
-    del = require('del'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
-    print = require('gulp-print'),
-    babel = require('gulp-babel');
-    //babel-preset-es2015
+  del = require('del'),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  print = require('gulp-print'),
+  babel = require('gulp-babel');
+//babel-preset-es2015
 
 
 var CacheBuster = require('gulp-cachebust');
@@ -21,45 +20,48 @@ var cachebust = new CacheBuster();
 
 const paths = {
   jsSource: ['./src/components/**/*.js', './src/components/*.js'],
-  cssFiles: './src/assets/scss/*.scss',
-  index: './src/index.html',
+  scssFiles: './src/assets/scss/*.scss',
+  cssFiles: './src/**/*.css',
+  indexFiles: './src/index.html',
   htmlFiles: './src/**/*.html',
   dist: './dist',
 };
 
-gulp.task('build-css', function(){
-    return gulp.src(paths.cssFiles)
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(cachebust.resources())
-        .pipe(concat('styles.css'))
-        .pipe(gulp.dest(paths.dist));
+gulp.task('build-css', function () {
+  return gulp.src([paths.scssFiles, paths.cssFiles])
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(cachebust.resources())
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('clean', function (cb) {
-    del([
-        'dist'
-    ], cb);
+  del([
+    'dist'
+  ], cb);
 });
 
-gulp.task('build-html', function(){
-    return gulp.src([paths.htmlFiles, paths.index])
+gulp.task('build-html', function () {
+  return gulp.src([paths.htmlFiles, paths.indexFiles])
     .pipe(gulp.dest(paths.dist))
 });
 
-gulp.task('build-js', function() {
+gulp.task('build-js', function () {
   return gulp.src(paths.jsSource)
-      .pipe(sourcemaps.init())
-      .pipe(print())
-      .pipe(babel({ presets: ['es2015'] }))
-      .pipe(concat('bundle.js'))
+    .pipe(sourcemaps.init())
+    .pipe(print())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(concat('bundle.js'))
     //   .pipe(uglify())
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest(paths.dist));
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('watch', function() {
-    return gulp.watch([paths.jsSource, paths.cssFiles, paths.index, paths.htmlFiles],['clean', 'build-css', 'build-js', 'build-html']);
+gulp.task('watch', function () {
+  return gulp.watch([paths.jsSource, paths.cssFiles, paths.scssFiles, paths.indexFiles, paths.htmlFiles], ['clean', 'build-css', 'build-js', 'build-html']);
 });
 
 gulp.task('default', ['clean', 'build-css', 'build-js', 'build-html', 'watch']);
