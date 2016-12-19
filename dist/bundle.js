40398e734aaca5f14af548b56f4bfe94f8ec47e2
Sterling Chin <sterling.chin@gmail.com>
2016-12-19 14:34:59 -0700

    added build to gulp tasks

---
+++ dist/bundle.js
@@ -0,0 +1,83 @@
+'use strict';
+
+angular.module('app', ['ui.router']).config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('home', {
+        url: '/',
+        templateUrl: './components/home/home.html',
+        controller: 'homeCtrl'
+    }).state('kata_list', {
+        url: '/kata_list',
+        templateUrl: './components/kata_list/kata_list.html',
+        controller: 'kata_listCtrl'
+    }).state('login', {
+        url: '/login',
+        templateUrl: './components/login/login.html',
+        controller: 'loginCtrl'
+    }).state('profile', {
+        url: '/profile',
+        templateUrl: './components/profile/profile.html',
+        controller: 'profileCtrl'
+    }).state('solutions', {
+        url: '/solutions',
+        templateUrl: './components/solutions/solutions.html',
+        controller: 'solutionsCtrl'
+    }).state('training', {
+        url: '/training',
+        templateUrl: './components/training/training.html',
+        controller: 'trainingCtrl'
+    });
+
+    $urlRouterProvider.otherwise('/');
+});
+'use strict';
+
+angular.module('app').directive('animateDir', function () {
+  return {
+    scope: {
+      settings: "="
+    },
+    restrict: 'EA',
+    link: function link(scope, elems, attrs) {
+      $(document).ready(function () {});
+    }
+  };
+});
+'use strict';
+
+angular.module('app').service('mainService', function ($http, $q, $sce) {
+
+  // $sce.trustAsResourceUrl('/s');
+
+  this.getTest = function (input) {
+    return $http({
+      method: 'POST',
+      url: '/solution',
+      data: {
+        script: input
+      }
+    });
+  };
+});
+'use strict';
+
+/***********HOME CONTROLLER***********/
+
+angular.module('app').controller('homeCtrl', function ($scope, $state, mainService) {});
+"use strict";
+"use strict";
+"use strict";
+"use strict";
+'use strict';
+
+/**********TRAINING CONTROLLER************/
+
+angular.module('app').controller('trainingCtrl', function ($scope, $state, mainService) {
+
+  $scope.sendSolution = function (input) {
+    // mainService.getTest(input).then((response) => console.log(response));
+    input = JSON.stringify(input);
+    input = input.replace("\n", " ");
+    console.log(input);
+  };
+});
+//# sourceMappingURL=bundle.js.map
