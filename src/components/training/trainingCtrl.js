33c128a23429ce8cbf09c89869059798feeddf7f
Steven Nagie <sanagie@yahoo.com>
2016-12-19 12:01:15 -0700

    Halto removing the escape characters.

--- src/components/training/trainingCtrl.js
+++ src/components/training/trainingCtrl.js
@@ -3,9 +3,12 @@
 angular.module('app').controller('trainingCtrl', function($scope, $state, mainService) {
 
   $scope.sendSolution = function(input) {
-    mainService.getTest(input).then((response) => console.log(response));
+    // mainService.getTest(input).then((response) => console.log(response));
+    input = JSON.stringify(input);
+    // input = input.replace("\n", " ");
+    console.log(input);
   }
 
-  
+
 
 });
