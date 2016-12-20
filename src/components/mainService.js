51680fd228f9cfab79445327e47e9d9cf116edde
Steven Nagie <sanagie@yahoo.com>
2016-12-19 18:03:53 -0700

    Added images that Jon made, worked on training page structure, and now have the 'run examples' button logging both the solution and the example exactly as we want it. All that's left on that end is to actually shoot it to the server in whatever sort of data layout that Josh wants.

--- src/components/mainService.js
+++ src/components/mainService.js
@@ -2,12 +2,12 @@ angular.module('app').service('mainService', function($http, $q, $sce) {
 
   // $sce.trustAsResourceUrl('/s');
 
-  this.getTest = function(input) {
+  this.getTest = function(solution, examples) {
     return $http({
       method: 'POST',
       url: '/solution',
       data: {
-        script: input
+        script: solution
       }
     });
   };
