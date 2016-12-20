d4546ce6c6f915eabfa18548e0e944c2f9e8092d
Sterling Chin <sterling.chin@gmail.com>
2016-12-20 14:00:12 -0700

    Ui-Views updated and children and parent views fixed;

--- src/components/app.js
+++ src/components/app.js
@@ -3,36 +3,47 @@ angular.module('app', ['ui.router'])
 
 .config(function($stateProvider, $urlRouterProvider){
 $stateProvider
-.state('home',{
-    url: '/',
-    templateUrl:'./components/home/home.html',
-    controller: 'homeCtrl'
-})
-.state('kata_list',{
-    url: '/kata_list',
-    templateUrl:'./components/kata_list/kata_list.html',
-    controller: 'kata_listCtrl'
-})
+
+//~~~~~~~~~~~~~Parent States~~~~~~~~~~~~~
+
 .state('login',{
     url: '/login',
     templateUrl:'./components/login/login.html',
-    controller: 'loginCtrl'
+    // controller: 'loginCtrl'
 })
-.state('profile',{
-    url: '/profile',
+.state('menu',{
+    url: '/',
+    templateUrl:'./components/menus/menu.html',
+    // controller: 'loginCtrl'
+})
+
+//~~~~~~~~~~~~~Child States~~~~~~~~~~~~~
+
+.state('menu.home',{
+    url: 'home',
+    templateUrl:'./components/home/home.html',
+    controller: 'homeCtrl'
+})
+.state('menu.kata_list',{
+    url: 'kata_list',
+    templateUrl:'./components/kata_list/kata_list.html',
+    controller: 'kata_listCtrl'
+})
+.state('menu.profile',{
+    url: 'profile',
     templateUrl:'./components/profile/profile.html',
     controller: 'profileCtrl'
 })
-.state('solutions',{
-    url: '/solutions',
+.state('menu.solutions',{
+    url: 'solutions',
     templateUrl:'./components/solutions/solutions.html',
-    controller: 'solutionsCtrl'
+    // controller: 'solutionsCtrl'
 })
-.state('training',{
-    url: '/training',
+.state('menu.training',{
+    url: 'training',
     templateUrl:'./components/training/training.html',
     controller: 'trainingCtrl'
 });
 
-$urlRouterProvider.otherwise('/');
+$urlRouterProvider.otherwise('/login');
 });
--- src/components/directive.js
+++ src/components/directive.js
@@ -5,7 +5,19 @@ angular.module('app').directive('animateDir', function () {
     },
     restrict: 'EA',
     link: function (scope, elems, attrs) {
-      $(document).ready(function () {})
+      $(document).ready(function () {
+        $('.side-menu').on('mouseenter', function () {
+          console.log('this is working');
+          $('.side-menu-container').css('width', '160px');
+        });
+        $('.side-menu-container').on('mouseleave', function () {
+          console.log("it's all gone");
+          $('.side-menu-container').css('width', '0px');
+        })
+      $('.top-menu').on('mouseenter', function(){
+        console.log('You have entered the twilight zone');
+      })
+      })
     }
   }
 });
