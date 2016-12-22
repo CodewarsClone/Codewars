bd2f8c6010df8f4a1b233cc2a777abbfbd150406
Sterling Chin <sterling.chin@gmail.com>
2016-12-22 11:01:14 -0700

    menu items set 1

--- src/components/directive.js
+++ src/components/directive.js
@@ -8,11 +8,14 @@ angular.module('app').directive('animateDir', function () {
       $(document).ready(function () {
         $('.side-menu').on('mouseenter', function () {
           console.log('this is working');
-          $('.side-menu-container').css('width', '160px');
+          // $('.side-menu-container').css('width', '160px');
+          $('.side-menu').css('width', '215px');
+          $('.menu-items').css('display','flex');
         });
-        $('.side-menu-container').on('mouseleave', function () {
+        $('.side-menu').on('mouseleave', function () {
           console.log("it's all gone");
-          $('.side-menu-container').css('width', '0px');
+          $('.side-menu').css('width', '55px');
+          $('.menu-items').css('display','none');
         });
         $('.top-menu').on('mouseenter', function () {
           console.log('You have entered the twilight zone');
