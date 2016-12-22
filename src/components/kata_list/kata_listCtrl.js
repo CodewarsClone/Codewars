9a87876925b48fce89a090f5739cd26cbb0ccae8
Steven Nagie <sanagie@yahoo.com>
2016-12-22 14:33:31 -0700

    Kata_list page complete for phase 2/MVP.

--- src/components/kata_list/kata_listCtrl.js
+++ src/components/kata_list/kata_listCtrl.js
@@ -1,5 +1,16 @@
 angular.module('app').controller('kata_listCtrl', function($scope, $state, mainService) {
 
+  $scope.tags = ["Fundamentals", "Data Types", "Algorithms", "Logic", "Strings", "Numbers", "Arrays", "Basic Language Features", "Mathematics", "Programming Paradigms", "Control Flow", "Declarative Programming", "Data Structures", "Puzzles", "Games", "Advanced Language Features", "Functions", "Regular Expressions", "Object-oriented Programming", "Lists", "Functional Programming", "Objects", "Bugs", "Utilities", "Arithmetic", "Loops", "Parsing", "Binary", "Integers", "Sorting", "Algebra", "Classes", "Formatting", "Theoretical Computer Science", "Dates/Time", "Security", "Higher-order Functions", "Prototypes", "Computability Theory", "Recursion", "Geometry", "Sequences", "Babel", "Cryptography", "ES2015", "Design Principles", "Graphs", "Hacking Holidays", "Design Patterns", "Validation"]
+
+  $scope.sortOptions = ["Newest", "Oldest", "Popularity", "Positive Feedback", "Most Completed", "Least Completed", "Recently Published", "Hardest", "Easiest", "Name"];
+  $scope.languageOptions = ["All", "My Languages", "C", "C++", "C#", "Clojure"];
+  $scope.statusOptions = ["Approved & Beta", "Approved", "Beta"];
+  $scope.progressOptions = ["All", "Kata I have not trained on", "Kata I have not completed", "Kata I have completed"];
+
+  $scope.searchFunction = (kata) => {
+    return kata.name === $scope.Filter;
+  }
+
     $scope.getRandomKataList = (userid) => {
         mainService.getRandomKataList(userid).then(response => {
             console.log(response.data);
@@ -18,7 +29,8 @@ angular.module('app').controller('kata_listCtrl', function($scope, $state, mainS
     $scope.getKatasByKyu = (kyu) => {
         mainService.getKatasByKyu(kyu).then(response => {
             console.log(response.data);
-            $scope.katasByKyu = response.data;
+            $scope.randomKataList = response.data;
+            $scope.totalKata = $scope.randomKataList.length;
         })
     }
 
