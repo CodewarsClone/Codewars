b1d0c2c21268b70834f234e84b842a4b32ca29fc
Steven Nagie <sanagie@yahoo.com>
2016-12-23 16:56:16 -0700

    Solutions page and color directive have most of the correct colors and directive is parsing stuff pretty perfectly.

--- src/components/solutions/colorDirective.js
+++ src/components/solutions/colorDirective.js
@@ -4,7 +4,9 @@ angular.module('app').directive('colorDirective', function() {
         var words = scope.solution.script.split(' ');
         let stringFlag = false;
         words = words.map((word, i) => {
-          if (stringFlag === true && (word.charAt(word.length - 1) === "'" || word.charAt(word.length - 1) === '"' || word.charAt(word.length - 1) === "`")) {
+          if ((word.charAt(word.length - 1) === "'" || word.charAt(word.length - 1) === '"' || word.charAt(word.length - 1) === "`") && (word.charAt(0) === '"' || word.charAt(0) === "'" || word.charAt(0) === "`")) {
+            return `<span style="color:#9fca56">${word}</span>`;
+          } else if (stringFlag === true && (word.charAt(word.length - 1) === "'" || word.charAt(word.length - 1) === '"' || word.charAt(word.length - 1) === "`")) {
            stringFlag = false;
            return `<span style="color:#9fca56">${word}</span>`;
          } else if (word.charAt(0) === '"' || word.charAt(0) === "'" || word.charAt(0) === "`") {
@@ -12,14 +14,14 @@ angular.module('app').directive('colorDirective', function() {
             return `<span style="color:#9fca56">${word}</span>`;
           } else if (stringFlag === true && word.charAt(word.length - 1) != "'"  && word.charAt(word.length -1) != '"' && word.charAt(word.length -1) != "`") {
             return `<span style="color:#9fca56">${word}</span>`;
-          } else if (typeof word === "number") {
-            return `<span style="color:#cd3f45">${word}</span>`;
           } else if (words[i-1] === "var" || words[i-1] === "let" || words[i-1] === "const") {
            return `<span style="color:#55b5db">${word}</span>`;
-         } else if (word === "var" || word === "let" || word === "const") {
+          } else if (word === "var" || word === "let" || word === "const") {
              return `<span style="color:#e6cd69">${word}</span>`
-           } else if (word === "function") {
+          } else if (word === "function") {
              return `<span style="color:#e6cd69">${word}</span>`;
+          } else if (typeof parseInt(word) === "number" && word != "=") {
+            return `<span style="color:#cd3f45">${word}</span>`;
           } else {
             return word;
           }
--- src/components/solutions/solutionsCtrl.js
+++ src/components/solutions/solutionsCtrl.js
@@ -5,37 +5,31 @@ angular.module('app').controller('solutionsCtrl', function($scope, $state, mainS
 
   $scope.kataid = $stateParams.kataid;
   $scope.user = mainService.user;
-  $scope.kataSolutions = [{ script:"function dog(x) { var jerk = 'hello there' }"}];
-
-  // var textareaSolutions = document.getElementById('solution-code-box');
-  // var solutionsPageCode = CodeMirror.fromTextArea(textareaSolutions, {
-  //  lineNumbers: true,
-  //  theme: 'seti',
-  // });
+  // $scope.kataSolutions = [{ script:"function dog(x) { var jerk = 'hello' }"}];
 
 
     $scope.getKataById = (kataid) => {
         mainService.getKataById(kataid).then(response => {
             console.log(response.data);
             $scope.kataById = response.data;
-            // $scope.getKataSolutions($scope.kataid);
+            $scope.getKataSolutions($scope.kataid);
             console.log($scope.kataById);
         })
     }
 
     var solutionsExist = false;
 
-    // $scope.getKataSolutions = (kataid) => {
-    //     mainService.getKataSolutions(kataid).then(response => {
-    //         $scope.kataSolutions = response.data;
-    //         if ($scope.kataSolutions[0]) {
-    //           // solutionsPageCode.setValue("Here's where your code should be.");
-    //         } else {
-    //           // solutionsPageCode.setValue("It looks like you haven't submitted any solutions to this kata.");
-    //         }
-    //         console.log($scope.kataSolutions)
-    //     })
-    // }
+    $scope.getKataSolutions = (kataid) => {
+        mainService.getKataSolutions(kataid).then(response => {
+            $scope.kataSolutions = response.data;
+            if ($scope.kataSolutions[0]) {
+              // solutionsPageCode.setValue("Here's where your code should be.");
+            } else {
+              // solutionsPageCode.setValue("It looks like you haven't submitted any solutions to this kata.");
+            }
+            console.log($scope.kataSolutions)
+        })
+    }
 
     $scope.init = () => {
         $scope.getKataById($scope.kataid);
