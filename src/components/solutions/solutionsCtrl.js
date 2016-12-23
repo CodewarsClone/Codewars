08e3f7ef5dd7967a05bf7c607cf786a9b39f03d4
Steven Nagie <sanagie@yahoo.com>
2016-12-23 10:32:13 -0700

    Basic layout of solutions page complete.

--- src/components/solutions/solutionsCtrl.js
+++ src/components/solutions/solutionsCtrl.js
@@ -4,6 +4,8 @@
 angular.module('app').controller('solutionsCtrl', function($scope, $state, mainService, $stateParams) {
 
   $scope.kataid = $stateParams.kataid;
+  $scope.user = mainService.user;
+  console.log(mainService.user);
 
     $scope.getKataById = (kataid) => {
         mainService.getKataById(kataid).then(response => {
@@ -16,12 +18,13 @@ angular.module('app').controller('solutionsCtrl', function($scope, $state, mainS
     $scope.getKataSolutions = (kataid) => {
         mainService.getKataSolutions(kataid).then(response => {
             $scope.kataSolutions = response.data;
+            $scope.kataSolutions[0] ? console.log("something's here") : console.log("We've got nothing");
             console.log($scope.kataSolutions)
         })
     }
 
     $scope.init = () => {
-        $scope.getKataById($scope.kataid); 
+        $scope.getKataById($scope.kataid);
     }
 
 
