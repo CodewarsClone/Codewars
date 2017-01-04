bfc0ab27829349903f59faadda9ea4c936e76076
Steven Nagie <sanagie@yahoo.com>
2017-01-03 17:15:36 -0700

    Proper filtering by difficulty. Icons highlight on mouseover and click. Tags working.

--- src/components/kata_list/kata_listCtrl.js
+++ src/components/kata_list/kata_listCtrl.js
@@ -8,6 +8,29 @@ angular.module('app').controller('kata_listCtrl', function($scope, $state, mainS
   $scope.statusOptions = ["Approved & Beta", "Approved", "Beta"];
   $scope.progressOptions = ["All", "Kata I have not trained on", "Kata I have not completed", "Kata I have completed"];
 
+  $scope.kyuIcons = [
+    "./assets/imgs/1_kyu_icon.png",
+    "./assets/imgs/2_kyu_icon.png",
+    "./assets/imgs/3_kyu_icon.png",
+    "./assets/imgs/4_kyu_icon.png",
+    "./assets/imgs/5_kyu_icon.png",
+    "./assets/imgs/6_kyu_icon.png",
+    "./assets/imgs/7_kyu_icon.png",
+    "./assets/imgs/8_kyu_icon.png",
+  ]
+
+  $scope.colorIcons = [  "./assets/imgs/1_kyu_icon_color.png", "./assets/imgs/2_kyu_icon_color.png",
+  "./assets/imgs/3_kyu_icon_color.png",   "./assets/imgs/4_kyu_icon_color.png", "./assets/imgs/5_kyu_icon_color.png",    "./assets/imgs/6_kyu_icon_color.png", "./assets/imgs/7_kyu_icon_color.png", "./assets/imgs/8_kyu_icon_color.png", ];
+
+  $scope.iconFlags = [];
+  $scope.iconDisplay = [];
+  $scope.setIconDisplay = () => {
+    for (let i = 0; i < 8; i++) {
+      $scope.iconFlags[i] = false;
+      $scope.iconDisplay[i] = $scope.kyuIcons[i];
+    }
+  }
+
   $scope.searchFunction = (kata) => {
     return kata.name === $scope.Filter;
   }
@@ -28,6 +51,10 @@ angular.module('app').controller('kata_listCtrl', function($scope, $state, mainS
     }
 
     $scope.getKatasByKyu = (kyu) => {
+        $scope.setIconDisplay();
+        $scope.iconFlags[kyu-1] = true;
+        console.log($scope.iconFlags[kyu-1]);
+        $scope.iconDisplay[kyu-1] = $scope.colorIcons[kyu-1];
         mainService.getKatasByKyu(kyu).then(response => {
             $scope.displayKataList = response.data;
             $scope.totalKata = $scope.displayKataList.length;
@@ -36,9 +63,11 @@ angular.module('app').controller('kata_listCtrl', function($scope, $state, mainS
 
     $scope.init = () => {
         $scope.getRandomKataList();
+        $scope.setIconDisplay();
     }
 
     $scope.tagClick = (filterThing) => {
+      $scope.setIconDisplay();
       $scope.displayKataList = [];
       for (let i = 0; i < $scope.randomKataList.length; i++) {
         if ($scope.randomKataList[i].tags.indexOf(filterThing.toUpperCase()) !== -1) {
