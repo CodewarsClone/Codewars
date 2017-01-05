1c9bf071e9c19c19a7c5f94c492e3b86550c0a4e
Steven Nagie <sanagie@yahoo.com>
2017-01-05 10:56:32 -0700

    Cleaned up difficulty icon code on kata_list page.

--- src/components/app.js
+++ src/components/app.js
@@ -4,9 +4,9 @@ angular.module('app', ['ui.router'])
 .config(function($stateProvider, $urlRouterProvider){
 $stateProvider
 
-import Rx from 'rxjs/Rx';
-
-Rx.Observable.of(1,2,3);
+// import Rx from 'rxjs/Rx';
+//
+// Rx.Observable.of(1,2,3);
 
 //~~~~~~~~~~~~~Parent States~~~~~~~~~~~~~
 
